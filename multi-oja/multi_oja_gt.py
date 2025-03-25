efac832005dc5c3d403f279dd89b07976cb8d354
Arjun Parthasarathy <56085610+ArjunParthasarathy@users.noreply.github.com>
2025-03-25 02:28:20 -0400

    wip multi oja gt - not converging

--- multi-oja/multi_oja_gt.py
+++ multi-oja/multi_oja_gt.py
@@ -7,7 +7,7 @@ gpu = torch.device("cuda:0")
 # Neurons in input layer
 N_x = 16
 # Neurons in output layer - leq to input layer neurons
-N_y = 4
+N_y = 2
 M = torch.normal(torch.zeros((N_x, N_x)), torch.ones((N_x, N_x)))
 Q, _ = torch.linalg.qr(M)
 Q = Q.to(gpu)
@@ -17,23 +17,23 @@ r_dist = torch.distributions.multivariate_normal.MultivariateNormal(torch.zeros(
 
 W = torch.normal(torch.zeros(N_y, N_x), torch.ones(N_y, N_x))
 W = W.to(gpu)
-W.requires_grad_(True)
+#W.requires_grad_(True)
 
 lr = 1e-3
 n_train = 2000
-alpha_start = 1e-2
+alpha_start = 1e-1
 alpha_end = 1e-3
 # Exponential decay lr starting at alpha_start and ending at alpha_end
 alphas = alpha_start*torch.exp(torch.arange(n_train) * (np.log(alpha_end / alpha_start) / (n_train-1)))
 #alphas = ((alpha_end - alpha_start) / n_train) * torch.arange(n_train) + alpha_start
 optim = torch.optim.Adam([W], lr=lr)
-# Batches to draw from normal dist to (noisily) estimate PC1
+# Batches to draw from normal dist to (noisily) estimate PC i
 losses = torch.empty((n_train,))
 #W_val = torch.empty(n_train, N)
 #pre = torch.empty((n_train, N))
 #post = torch.empty((n_train, 1))
 for i in range(n_train):
-    r_pre = r_dist.sample()
+    r_pre = r_dist.sample().unsqueeze(1)
     r_post = W @ r_pre
     
     B = 512
@@ -41,11 +41,13 @@ for i in range(n_train):
     eigvals, eigvecs = torch.linalg.eigh(c)
     pc_i = eigvecs[:, -N_y:].t()
 
+    #print(eigvals[-N_y:], torch.exp(-torch.arange(N_x))[:N_y])
+
     norm_pci = pc_i / torch.linalg.vector_norm(pc_i, dim=1).unsqueeze(1)
     norm_W = W / torch.linalg.vector_norm(W, dim=1).unsqueeze(1)
 
     # transposing and unsqueezing to match required bmm format
-    a = torch.bmm(norm_W.unsqueeze(1), norm_pci.unsqueeze(2)).squeeze(1, 2)
+    a = torch.bmm(norm_W.unsqueeze(1), norm_pci.unsqueeze(2))
 
     loss = 1-torch.mean(torch.abs(a))
 
@@ -54,16 +56,16 @@ for i in range(n_train):
     # W_val[i] = W
     # pre[i] = r_pre
     # post[i] = r_post
-    #delta_W = alphas[i] * r_post*(r_pre - r_post*W)
-    #W += delta_W
-    optim.zero_grad()
-    loss.backward()
-    optim.step()
+    delta_W = alphas[i] * r_post*(r_pre.t() - r_post*W)
+    W += delta_W
+    # optim.zero_grad()
+    # loss.backward()
+    # optim.step()
 
 
 plt.plot(losses)
-plt.savefig("losses_gd.png")
-torch.save(losses, "losses_gd.pt")
+plt.savefig("losses_gt.png")
+torch.save(losses, "losses_gt.pt")
 # torch.save(pre, "r_pre_gd.pt")
 # torch.save(post, "r_post_gd.pt")
 # torch.save(W_val, "weights_gd.pt")
